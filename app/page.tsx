eac966fb55147f297ddef9427b7dbeaf2c3d038e
SanketLakhani <sanketlakhani80@gmail.com>
2023-09-19 16:23:48 +0530

    board, column and todo card added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
       <Header />
 
       {/* Board */}
-      <h1>Trello 2.0 AI Clone</h1>
+      <Board />
     </main>
   );
 }
